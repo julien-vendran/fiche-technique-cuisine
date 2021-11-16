bea5ad7a1f2604b1de5635caadd36c19af46254e
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2021-11-16 17:52:44 +0100

    Mise en place de la connexion avec la base de données. Seul la connexion est faite pour le moment

--- fiche-technique-cuisine/src/app.module.ts
+++ fiche-technique-cuisine/src/app.module.ts
@@ -1,10 +1,29 @@
+import { ConfigModule } from '@nestjs/config';
+
+import { HomeController } from './home.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './process.env',
+    }),
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL, 
+      type: 'postgres',
+      ssl: {
+        rejectUnauthorized: false,
+      }, 
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // This for development
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [
+    HomeController, AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
