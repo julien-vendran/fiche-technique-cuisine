8beff6bfbdc25a60be464ee4b2cb8a306dd73db6
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2021-12-08 21:17:47 +0100

    Mise à jour du composants de création d'ingrédient

--- fiche-technique-cuisine/frontend/src/app/component/ingredient/create-ingredient/create-ingredient.component.ts
+++ fiche-technique-cuisine/frontend/src/app/component/ingredient/create-ingredient/create-ingredient.component.ts
@@ -1,52 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 import { Ingredient } from '../../../model/ingredient'
+import { Allergen } from '../../../model/allergen';
+
+import * as M from 'materialize-css'; 
+import { IngredientService } from '../../../service/ingredient.service';
+import { AllergenService } from '../../../service/allergen.service';
 
 @Component({
   selector: 'app-create-ingredient',
   templateUrl: './create-ingredient.component.html',
   styleUrls: ['./create-ingredient.component.css']
 })
-export class CreateIngredientComponent implements OnInit {
+export class CreateIngredientComponent implements OnInit, AfterViewInit{
 
-  ingredientGroup : FormGroup = new FormGroup({}); 
-  private ingredient: Ingredient | null = null;
-  private backend_url = "http://localhost:3000/ingredients"
+  public ingredientGroup : FormGroup | null = null; 
+  public ingredient: Ingredient = new Ingredient();
+  public allergens_list : Allergen[] = []; 
 
-  constructor (private http: HttpClient, private fb: FormBuilder) {
-    this.ingredientGroup = this.fb.group({
-      name: [this.ingredient?.name]
-    }); 
+  constructor (
+    private fb: FormBuilder,
+    private ingredientService: IngredientService, 
+    private allergenService: AllergenService) {
+     
   }
 
   validate(): void {
     console.log("Validation in progress ..."); 
-    if (this.ingredient && this.ingredientGroup)
-      this.ingredient.name = this.ingredientGroup.get('name')?.value;
-      
-    this.send();
-  }
 
-  send(): void {
-    console.log("Send in progress ..."); 
-    this.http.post<Ingredient>(
-      this.backend_url, 
-      this.ingredient?.toJSON
-    );
+    //On va créer un ingrédient avec les éléments qu'on a eu
+    if (this.ingredientGroup) {
+      if (  this.ingredientGroup.get('allergens') != null) {
+        let tab_allergens: Allergen[] = []; 
+        this.ingredientGroup.get('allergens')?.value.array.forEach((all_id: any) => {
+          this.allergenService.getAllergen(all_id).subscribe( (data) => 
+            tab_allergens.push(new Allergen(data.id_Allergen, data.allergen_name))
+          ); 
+        });
+      }
+      this.ingredient = new Ingredient(
+        this.ingredientGroup.get('name')?.value, 
+        this.ingredientGroup.get('unit')?.value, 
+        this.ingredientGroup.get('availableQuantity')?.value, 
+        this.ingredientGroup.get('unitPrice')?.value, 
+        this.ingredientGroup.get('allergens')?.value
+      );
+
+      console.log("C'est envoyé !");
+      console.log(JSON.stringify(this.ingredient));
+      console.log('-----------');
+
+      //Envoie des données
+      this.ingredientService.createIngredient(this.ingredient);
+    }
+    //On remet à zéro notre ingrédient
+    this.ingredient = new Ingredient();
   }
 
   ngOnInit (): void {
-    console.log("Init in progress ..."); 
-    this.ingredient = new Ingredient("", "litre", 200, 1.2, false); 
+    this.ingredientGroup = this.fb.group({
+      name: [this.ingredient?.name], 
+      unit: [this.ingredient?.unit], 
+      availableQuantity: [this.ingredient?.availableQuantity], 
+      unitPrice: [this.ingredient?.unitPrice], 
+      allergens: [this.ingredient?.allergens]
+    });
+
+    this.allergenService.getAllAlergen().subscribe(data => {
+      this.allergens_list = data; 
+      setTimeout(this.initSelectMaterialize, 100);
+    });
   }
 
-  // getAllIngredient() : void {
-  //   this.http.get<any>(this.backend_url + 'ingredients').subscribe(data => {
-  //     this.ingredients = JSON.stringify(data);
-  //   }); 
-  // }
+  ngAfterViewInit (): void {
+    this.initSelectMaterialize();
+  }
 
+  initSelectMaterialize (): void {
+    let options:any = {isMultiple: true}; 
+    M.FormSelect.init(document.querySelectorAll('select'), options);
+  }
+
+  change($event: any): void {
+    this.initSelectMaterialize();
+  }
 }
