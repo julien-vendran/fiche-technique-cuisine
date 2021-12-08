583a854563f44cdceb4bf0a14fac262d2f67e4ff
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2021-12-08 21:15:39 +0100

    Suppression de 'is-allergenic' pour une table à la place

--- fiche-technique-cuisine/src/is-allergenic/dto/update-is-allergenic.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIsAllergenicDto } from './create-is-allergenic.dto';
-
-export class UpdateIsAllergenicDto extends PartialType(CreateIsAllergenicDto) {}
--- fiche-technique-cuisine/src/is-allergenic/is-allergenic.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IsAllergenicService } from './is-allergenic.service';
-import { IsAllergenicController } from './is-allergenic.controller';
-
-@Module({
-  controllers: [IsAllergenicController],
-  providers: [IsAllergenicService]
-})
-export class IsAllergenicModule {}
