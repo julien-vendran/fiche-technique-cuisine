95ba90c6909d3203c80d88f99fdb7672d21f6dae
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2021-12-18 22:43:48 +0100

    Ajout de la partie recette

--- fiche-technique-cuisine/frontend/src/app/model/recipe.ts
+++ fiche-technique-cuisine/frontend/src/app/model/recipe.ts
@@ -1,2 +1,25 @@
-export class Recipe {
+import { RecipeOrStep } from "./recipe-or-step";
+
+export class Recipe extends RecipeOrStep {
+
+    listOfSteps: RecipeOrStep[];
+    responsable: String;
+    nbOfCover: number;
+    category: String;
+
+    constructor(
+        name: String, 
+        listOfSteps: RecipeOrStep[], 
+        responsable: String,
+        nbOfCover: number, 
+        category: String,  
+        id?: number
+    ) {
+        super(name, id); 
+
+        this.listOfSteps = listOfSteps; 
+        this.responsable = responsable; 
+        this.nbOfCover = nbOfCover; 
+        this.category = category; 
+    }
 }
