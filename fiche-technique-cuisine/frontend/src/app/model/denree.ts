4df0b0f4a2178240a9ec2f24902a95cf7863e865
Julien Vendran <47184210+julien-vendran@users.noreply.github.com>
2022-01-06 09:30:10 +0100

    Mise à jour des entités côté front

--- fiche-technique-cuisine/frontend/src/app/model/denree.ts
+++ fiche-technique-cuisine/frontend/src/app/model/denree.ts
@@ -1,16 +1,20 @@
-import {Ingredient} from "./ingredient";
+import { Ingredient } from "./ingredient";
+import { Step } from "./step";
 
 export class Denree {
-  id:number | undefined;
-  quantity:number =0;
-  ingredient:Ingredient | undefined;
+  id: number | null = null;
+  quantity: number = 0;
+  ingredient: Ingredient | null = null;
+  step: Step | null = null;
   constructor(
-    quantity?:number,
-    ingredient?:Ingredient,
-    id?:number
-  ){
-    this.id=id;
-    this.quantity= quantity ? quantity:0;
-    this.ingredient=ingredient ? ingredient: undefined;
+    quantity?: number,
+    ingredient?: Ingredient,
+    step?: Step,
+    id?: number
+  ) {
+    this.id = id ? id : null;
+    this.quantity = quantity ? quantity : 0;
+    this.ingredient = ingredient ? ingredient : null;
+    this.step = step ? step : null;
   }
 }
